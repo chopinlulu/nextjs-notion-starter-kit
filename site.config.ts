88fe85b015e98ab19d74fa66986cda61a6b8aca8
chopinlulu <82205633+chopinlulu@users.noreply.github.com>
2024-03-04 16:11:15 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'dc64bd277239477f9949e470d1cc6a55?v=8f258a885ffe415c9521be2f056bf1d6',
+  rootNotionPageId: '7875426197cf461698809def95960ebf',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Lulu's test site',
-  domain: 'https://friendly-bill-024.notion.site/dc64bd277239477f9949e470d1cc6a55?v=8f258a885ffe415c9521be2f056bf1d6',
-  author: 'Lulu Chen',
+  name: 'L Test',
+  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  author: 'xxxxxxxx',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
